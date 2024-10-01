a480c6a5841d9c45c648d621a91d5c710ca8e0c4
OlesyaEfr <olesya_s89@mail.ru>
2024-10-01 19:18:18 +0300

    all tests

--- test_ui.py
+++ test_ui.py
@@ -21,18 +21,25 @@ def driver():
 
 
 #1.Ввод в поисковой строке существующего товара на сайте
-#driver.find_element(By.TAG_NAME, input).send_keys('python')
-#driver.find_element(By.XPATH, html[1]body[1]div[2]div[1]div[1]header[1]div[1]div[1]div[2]div[1]form[1]button[1][name()='svg'][1]).click()
-
-#sleep(5)
+def test_pozitive_search(driver):
+    driver.get('https://www.chitai-gorod.ru')
+    driver.find_element(By.CLASS_NAME, "header-search__input").send_keys('шариковая ручка')
+    driver.find_element(By.CLASS_NAME, "header-search__button-icon").click()
+    sleep(3)
 
 #2.Поиск конкретной книги, которой нет в наличии, и добавление одной из списка в подписки
-#driver.find_element(By.CLASS_NAME, header-search__clear-icon).click()
-#driver.find_element(By.TAG_NAME, input).send_keys('Терри Пратчетт Эрик')
-#driver.find_element(By.XPATH, html[1]body[1]div[2]div[1]div[1]header[1]div[1]div[1]div[2]div[1]form[1]button[1][name()='svg'][1]).click()
-#driver.find_element(By.CSS_SELECTOR, ) Как определить первую из списка и нажать на кнопку В подписке
-                   
-#sleep(5)
+def test_bookmarks(driver):
+    driver.get('https://www.chitai-gorod.ru')
+    driver.find_element(By.CLASS_NAME, "header-search__input").send_keys('Терри Пратчетт Эрик')
+    driver.find_element(By.CLASS_NAME, "header-search__button-icon").click()
+    sleep(3)
+    # Ожидание загрузки элемента 
+    element = WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.XPATH, '/html[1]/body[1]/div[2]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/section[1]/section[1]/div[1]/article[1]/div[3]/div[1]')) )
+    # Скролл к элементу
+    driver.execute_script("arguments[0].scrollIntoView(true);", element)
+    # Клик по элементу 
+    ActionChains(driver).move_to_element(element).click(element).perform()
+    sleep(3)
 
 #3. Поиск конкретной книги и добавление в корзину
 def test_buy_book(driver):
@@ -44,28 +51,50 @@ def test_buy_book(driver):
     driver.find_element(By.CSS_SELECTOR, "img[alt='Букварь: Учебное пособие.']").click()
     sleep(3)
     # Ожидание загрузки элемента 
-    element = WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.XPATH, '/html[1]/body[1]/div[2]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/section[1]/section[1]/div[1]/article[1]/div[3]/div[1]')) )
+    element = WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.XPATH, '/html[1]/body[1]/div[2]/div[1]/div[1]/div[3]/main[1]/div[1]/div[6]/div[2]/div[1]/div[1]/div[2]/button[1]')) )
     # Скролл к элементу
     driver.execute_script("arguments[0].scrollIntoView(true);", element)
     # Клик по элементу 
     ActionChains(driver).move_to_element(element).click(element).perform()
     sleep(3)
 
-#def test_buy_book_verify(driver):
-    #driver.get('https://www.chitai-gorod.ru')
-    #driver.find_element(By.CLASS_NAME, "header-search__input").send_keys('букварь учебное пособие. надежда жукова')
-    #driver.find_element(By.CLASS_NAME, "header-search__button-icon").click()
-
-    #sleep(3)
-    #driver.find_element(By.XPATH, "/html[1]/body[1]/div[2]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/section[1]/section[1]/div[1]/article[1]/a[1]/picture[1]/img[1]").click()
-    #sleep(3)
-    # Ожидание загрузки элемента 
-    #element = WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.XPATH, '/html[1]/body[1]/div[2]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/section[1]/section[1]/div[1]/article[1]/div[3]/div[1]')) )
-    # Скролл к элементу
-    #driver.execute_script("arguments[0].scrollIntoView(true);", element)
-    # Клик по элементу 
-    #ActionChains(driver).move_to_element(element).click(element).perform()
+#4. Проверка, что книга добавилась в корзину
+def test_buy_book(driver):
+    driver.get('https://www.chitai-gorod.ru')
+    driver.find_element(By.CLASS_NAME, "header-search__input").send_keys('букварь учебное пособие. надежда жукова')
+    driver.find_element(By.CLASS_NAME, "header-search__button-icon").click()
 
     sleep(3)
+    driver.find_element(By.CSS_SELECTOR, "img[alt='Букварь: Учебное пособие.']").click()
+    sleep(3)
+    # Ожидание загрузки элемента 
+    element = WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.XPATH, '/html[1]/body[1]/div[2]/div[1]/div[1]/div[3]/main[1]/div[1]/div[6]/div[2]/div[1]/div[1]/div[2]/button[1]')) )
+    # Клик по элементу 
+    ActionChains(driver).move_to_element(element).click(element).perform()
+    sleep(3)
+    # Переход в корзину
+    driver.find_element(By.CSS_SELECTOR, ".header-cart.sticky-header__controls-item").click()
+    sleep(3)
+    # Проверим, что в корзине товаров больше 0
+    goods = driver.find_elements(By.CSS_SELECTOR, "div.cart-item")
+    assert goods > 0
 
-#4. Поиск конкретной книги, которой нет в наличии, и добавление одной из списка в подписки
+#5. Очистка корзины
+def test_delete_goods_from_cart(driver):
+    driver.get('https://www.chitai-gorod.ru')
+    driver.find_element(By.CLASS_NAME, "header-search__input").send_keys('букварь учебное пособие. надежда жукова')
+    driver.find_element(By.CLASS_NAME, "header-search__button-icon").click()
+
+    sleep(3)
+    driver.find_element(By.CSS_SELECTOR, "img[alt='Букварь: Учебное пособие.']").click()
+    sleep(3)
+    # Ожидание загрузки элемента 
+    element = WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.XPATH, '/html[1]/body[1]/div[2]/div[1]/div[1]/div[3]/main[1]/div[1]/div[6]/div[2]/div[1]/div[1]/div[2]/button[1]')) )
+    # Клик по элементу 
+    ActionChains(driver).move_to_element(element).click(element).perform()
+    sleep(3)
+    # Переход в корзину
+    driver.find_element(By.CSS_SELECTOR, ".header-cart.sticky-header__controls-item").click()
+    sleep(3)
+    driver.find_element(By.CSS_SELECTOR, ".delete-many").click()
+    sleep(3)
